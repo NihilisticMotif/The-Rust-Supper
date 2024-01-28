a1fcd5aaa0912bc00d58f1ad59f72cc96a1e1518
Nihilisticmotif <nemocheche@gmail.com>
2024-01-28 17:23:44 +0700

    I learn about struct and cons list
    I will continue watching the Rust tutorial.

--- TheRustSupper/src/main.rs
+++ TheRustSupper/src/main.rs
@@ -1,21 +1,12 @@
 #![allow(unused)]
-// #[derive(Debug)]
-use std::fmt;
-// https://youtu.be/BpPEoZW5IiY?si=goDW5z6qEkPok4pe
 // #[derive(Debug)] // used for debugging
 
-enum Operation{
-    Add,
-    Sub,
-    
-}
-
-struct Diculator{
-    num1:i32,
-    num2:i32,
-}
+// https://prince.dev/debug-struct
+// https://youtu.be/BpPEoZW5IiY?si=goDW5z6qEkPok4pe
+use std::fmt;
 
 fn main() {
+
 }
 
 /*
