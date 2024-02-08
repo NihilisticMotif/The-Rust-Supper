04de90490a78cfeb37c9ff2e73312fe3185a9853
Nihilisticmotif <nemocheche@gmail.com>
2024-02-08 14:27:08 +0700

    I will learn data structure.

--- the_rust_supper/src/main.rs
+++ the_rust_supper/src/main.rs
@@ -1,12 +1,44 @@
 #![allow(unused)]
 // https://stackoverflow.com/questions/37410672/expected-type-parameter-found-u8-but-the-type-parameter-is-u8
-use std::fmt;
-use std::fmt::Display;
-use std::fmt::Debug;
-use generic_array::{GenericArray, ArrayLength,arr};
-use std::iter::Iterator;
-use std::marker::Copy;
-use std::default::Default;
+// use std::fmt::{self, Debug};
+// https://prince.dev/debug-struct
+// https://youtu.be/BpPEoZW5IiY?si=goDW5z6qEkPok4pe
+// use std::fmt;
+//**************************************************************************************************
+// EXAMPLE NO.1 METHOD
+//**************************************************************************************************
+struct A;          // Concrete type `A`.
+struct S(A);       // Concrete type `S`.
+struct SGen<T>(T); // Generic type `SGen`.
+
+fn reg_fn(_s: S) {}
+
+fn gen_spec_t(_s: SGen<A>) {}
+
+fn gen_spec_i32(_s: SGen<i32>) {}
+
+fn generic<T>(_s: SGen<T>) {}
+
+fn main() {
+//**************************************************************************************************
+// EXAMPLE NO.1 METHOD
+//**************************************************************************************************
+// Using the non-generic functions
+reg_fn(S(A));          // Concrete type.
+gen_spec_t(SGen(A));   // Implicitly specified type parameter `A`.
+gen_spec_i32(SGen(4)); // Implicitly specified type parameter `i32`.
+// Explicitly specified type parameter `char` to `generic()`.
+generic::<char>(SGen('a'));
+// Implicitly specified type parameter `char` to `generic()`.
+generic(SGen('a'));
+println!("Success!");
+
+//**************************************************************************************************
+// EXAMPLE NO.1 METHOD
+//**************************************************************************************************
+
+
+}
 
 /*
 cargo run
